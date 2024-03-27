3c51dbd2c40f104fc1500baf5fc2e520d62a0333
Maximiliano Furtado <maximiliano.oliveira.furtado@gmail.com>
2024-03-27 07:48:58 -0300

    refactor: operation controllet

--- cmd/rest/operation_tax_controller.go
+++ cmd/rest/operation_tax_controller.go
@@ -8,25 +8,28 @@ import (
 )
 
 type IController interface {
-	PostTaxOperation(_ context.Context, []entity.Operation) ([]entity.Tax, error)
+	PostTaxOperation(_ context.Context, _ []entity.Operation) ([]entity.Tax, error)
 }
 
 type operationTaxController struct {
 	operationService operation.IService
 }
 
-func NewOperationTaxController(service operation.IService) IController {
+func NewOperationTaxController(operationService operation.IService) IController {
 	return &operationTaxController{
 		operationService: operationService,
 	}
 }
 
 func (c *operationTaxController) PostTaxOperation(ctx context.Context, operations []entity.Operation) ([]entity.Tax, error) {
+
+	var operationsTaxes []entity.Tax
+
 	for _, operationInputed := range operations {
-		operationTax := operationService.OperationTax(&operationInputed)
+		operationTax := c.operationService.OperationTax(&operationInputed)
 		operationsTaxes = append(operationsTaxes, *operationTax)
 	}
 
-	return operationsTaxes
+	return operationsTaxes, nil
 
 }
